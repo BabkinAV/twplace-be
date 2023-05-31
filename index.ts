7736c5df3e30e3a37cf03550c5a6e93d0db352e3
Andrey Babkin <babkin.andrey@gmail.com>
2023-05-31 11:35:46 +0300

    initialized graphql

--- index.ts
+++ index.ts
@@ -1,11 +1,37 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Express } from 'express';
+import 'dotenv/config'
+
+import { graphqlHTTP } from 'express-graphql';
+import { buildSchema } from 'graphql';
+
+const port = process.env.PORT  || 5000;
 
 const app: Express = express();
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server!!!');
-});
+// Construct a schema, using GraphQL schema language
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`)
 
-app.listen(8080, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${8080} !!!`);
+// The root provides a resolver function for each API endpoint
+const root = {
+  hello: () => {
+    return "Hello world!"
+  },
+
+}
+
+
+
+app.use("/graphql",
+graphqlHTTP({
+	schema: schema,
+	rootValue: root,
+	graphiql: true,
+}))
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${port} !!!`);
 });
