961f06659dd95ea3bb602c575be6c69e4e9d004a
Andrey Babkin <babkin.andrey@gmail.com>
2023-05-31 16:26:24 +0300

    added docker container

--- index.ts
+++ index.ts
@@ -26,7 +26,7 @@ const schema = buildSchema(`
 // The root provides a resolver function for each API endpoint
 const root = {
   hello: () => {
-    return "Hello world!"
+    return "Hello world!!!"
   },
 
 }
--- server/config/db.ts
+++ server/config/db.ts
@@ -1,10 +1,11 @@
 import mongoose from 'mongoose';
+import  'colors'
 
 const connectDB = async () => {
   mongoose.set('strictQuery', true);
   const conn = await mongoose.connect(process.env.MONGO_URI!);
 
-  console.log(`MongoDB Connected: ${conn.connection.host}`);
+  console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
 };
 
 export default connectDB;
