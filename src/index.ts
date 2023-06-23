58a1ed08b6725e0cc666d615834681a680f8eeec
Andrey Babkin <babkin.andrey@gmail.com>
2023-06-23 22:10:18 +0300

    change

--- src/index.ts
+++ src/index.ts
@@ -47,5 +47,5 @@ app.get('/test', (req: Request, res: Response, next) => {
 });
 
 app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port} !`);
+  console.log(`⚡️[server]: Server 6 is running at http://localhost:${port} !`);
 });
